f2fc80fa63a6770e60e8f147e63072e6892678d2
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2023-07-13 20:49:01 +0530

    Update prog19.java

--- prog19.java
+++ prog19.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 public class prog19 
 {
     public static void main(String args[]){
-        System.out.println("Enter 2 numbers :");
+        System.out.println("Enter a number :");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
         for (int i=1; i<=a; i++)
         {
             for(int j=1;j<=i;j++)

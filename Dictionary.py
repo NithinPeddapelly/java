aad25139548f8ba52822e9952d230a2eb8b8fdcd
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2023-07-31 19:38:05 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+Hokage = {"Hashirama" : 1 ,"Tobirama":2 ,"Sarutobi" : 3 , "Minato":4 , "Tsunade" : 5 , "Kakashi" : 6 }
+print (Hokage)
+print(Hokage["Hashirama"]) # returns 1 which is assigned value ':' for Key-Hashirama 
+Hokage["Naruto"] = 7 ; # for adding values to the dictionary we use [ "key" ] = value ;
+print(Hokage["Naruto"])
+Hokage["Kakashi"] =  99; # used for updating the values of keys.
+print(Hokage["Kakashi"])
